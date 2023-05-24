b081635cf7f5089366adcd275042cf445fa07849
conggaro <conggaro@gmail.com>
2023-05-24 12:19:06 +0700

    upload source code

---
+++ Do toc do Internet/Do toc do Internet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.NetworkInformation;
+
+namespace NetworkSpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing network speed...");
+            Ping ping = new Ping();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            PingReply pingReply = ping.Send("www.google.com");
+            stopwatch.Stop();
+            Console.WriteLine($"Ping time: {pingReply.RoundtripTime} ms");
+            Console.WriteLine($"Download speed: {Math.Round(1000.0 / stopwatch.ElapsedMilliseconds * 8 * pingReply.Buffer.Length / 1024 / 1024, 2)} Mbps");
+        }
+    }
+}
